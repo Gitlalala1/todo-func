1d0fe9306ebdb36c1e91b641aa324302f008cf2a
Gitlalala1 <gitbumlala@mail.ru>
2023-05-02 16:12:41 +0300

    changed title

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,15 +1,15 @@
 import React from "react";
-import './header.css'
+import "./header.css";
 
-const Header=({count,done})=>{
-    return(
-        <div className="header-app">
-            <h2>This is Todo</h2>
-            <div>
-            <span>Count:{count}</span>
-            <span>Done:{done}</span>
-            </div>
-        </div>
-    )
-}
-export default Header
+const Header = ({ count, done }) => {
+	return (
+		<div className="header-app">
+			<h2>This is my todo list</h2>
+			<div>
+				<span>Count:{count}</span>
+				<span>Done:{done}</span>
+			</div>
+		</div>
+	);
+};
+export default Header;
